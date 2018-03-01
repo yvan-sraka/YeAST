392774e395bddf36b9df5d4c91d9d39e5c28be76
Yvan Sraka <yvan.sraka@gmail.com>
2018-03-01 16:14:08 +0100

    First working version of YeAST, following a simple block behavior

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn pipe(uuid: &str) -> String {
     let path = format!("/tmp/yeast_{}", uuid);
     let filename = CString::new(path.clone()).unwrap();
     unsafe { libc::mkfifo(filename.as_ptr(), 0o644); }
-    path
+    return path;
 }
 
 fn exec(bin: &str, cin: &str) -> String {
@@ -30,27 +30,42 @@ fn exec(bin: &str, cin: &str) -> String {
     format!("{}", String::from_utf8_lossy(&output.stdout))
 }
 
-fn rec(contents: &str) -> String {
-    let s = contents.find("#!").unwrap();
-    let e = contents[s..].find("\n").unwrap();
+fn rec(contents: &str, l: u32) -> (String, u32) {
+    let mut output = String::from("");
+    let mut i = 0;
+    let mut j = l;
+    for line in contents.lines() { if i >= j {
+        if line.contains("#!") {
+            let (x, y) = rec(contents, i + 1);
+            let cmd = String::from(&line[line.find("#!").unwrap()+2..]);
+            let input = x.clone();
 
-    let cmd = String::from(&contents[s+2..e]);
-    let input = String::from(&contents[e+1..]);
+            let cin = pipe(&uuid());
+            let cin2 = cin.clone(); // TODO
 
-    let cin = pipe(&uuid());
-    let cin2 = cin.clone(); // TODO
+            let b = thread::spawn(move || {
+                let mut file = OpenOptions::new().write(true).open(cin2)
+                        .expect("ERROR");
+                file.write_all(input.as_bytes()).expect("ERROR");
+            });
 
-    let b = thread::spawn(move || {
-        let mut f = OpenOptions::new().write(true).open(&cin2).expect("ERROR");
-        f.write_all(input.as_bytes()).expect("ERROR");
-    });
+            let a = thread::spawn(move || -> String {
+                exec(&cmd, &cin)
+            });
 
-    let a = thread::spawn(move || -> String {
-        exec(&cmd, &cin)
-    });
+            b.join().expect("ERROR");
 
-    b.join().expect("ERROR");
-    return a.join().unwrap();
+            output += "\n";
+            output += &a.join().unwrap();
+
+            j = y;
+        } else if line.contains("!#") {
+            return (output, i + 1);
+        } else {
+            output += line;
+        }
+    } i += 1; } // UGLY
+    return (output, i);
 }
 
 fn main() {
@@ -58,7 +73,6 @@ fn main() {
     let filename = &args[1];
     let mut file = File::open(filename).expect("file not found");
     let mut contents = String::new();
-    file.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-    print!("{}", rec(&contents));
+    file.read_to_string(&mut contents).expect("error reading the file");
+    print!("{}", rec(&contents, 0).0);
 }
