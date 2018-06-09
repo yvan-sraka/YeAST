df62e09b724128ecb6eb60199dac7f0f90ab3159
Yvan Sraka <yvan@sraka.pw>
2018-06-09 23:41:53 +0200

    Restart with a fresh Rust base

--- Modules/c++/cpp.cc
+++
@@ -1,9 +0,0 @@
-#include "cpp.hh"
-
-Cpp::Cpp(std::string environment)
-:Bloc(environment) {
-}
-
-void Cpp::feed(const std::string &code) {
-    this->code_ += code + "\n";
-}
--- Modules/trailing_whitespaces/main.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::string str;
-    std::cin >> str;
-    for (size_t pos = 0; (pos = str.find("\n", pos)) != std::string::npos; pos += 5)
-
-    std:cout << str;
-}
