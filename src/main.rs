92e04e2174601bf4a5c41c27cd9598caae8347b2
Yvan Sraka <yvan.sraka@gmail.com>
2018-12-14 17:15:58 +0100

    Fix lints given by Clippy

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,12 @@ fn err(msg: &str) -> String {
     format!("\n\n\x1b[31merror:\x1b[0m {}\n", msg)
 }
 
-fn exec(cmd: String, input: String) -> String {
+fn exec(cmd: &str, input: String) -> String {
     // Create a named pipe using libc (see man 2 mkfifo)
-    std::fs::create_dir_all("/tmp/yeast/").expect(&err("couldn't create the /tmp/yeast/ folder"));
+    std::fs::create_dir_all("/tmp/yeast/").unwrap_or_else(|_| {
+        let msg = err("couldn't create the /tmp/yeast/ folder");
+        panic!(msg)
+    });
     let uuid: String = rand::thread_rng().gen_ascii_chars().take(64).collect();
     let path = format!("/tmp/yeast/{}", uuid);
     let filename = std::ffi::CString::new(path.clone()).unwrap();
@@ -26,9 +29,14 @@ fn exec(cmd: String, input: String) -> String {
         let mut file = std::fs::OpenOptions::new()
             .write(true)
             .open(cout)
-            .expect(&err("couldn't open the yeast named pipe")); // PANIC
-        file.write_all(input.as_bytes())
-            .expect(&err("couldn't write the yeast named pipe")); // PANIC
+            .unwrap_or_else(|_| {
+                let msg = err("couldn't open the yeast named pipe");
+                panic!(msg)
+            });
+        file.write_all(input.as_bytes()).unwrap_or_else(|_| {
+            let msg = err("couldn't write the yeast named pipe");
+            panic!(msg)
+        });
     }); // This thread don't need to be join
     let cmd = if cmd.contains("$0") {
         str::replace(&cmd, "$0", &cin)
@@ -40,7 +48,10 @@ fn exec(cmd: String, input: String) -> String {
     let output = std::process::Command::new("sh")
         .args(&["-c", &sh])
         .output()
-        .expect(&err(&format!("failed to execute process: {}", sh)));
+        .unwrap_or_else(|_| {
+            let msg = err(&format!("failed to execute process: {}", sh));
+            panic!(msg)
+        });
     if output.status.success() {
         // Retreive output of shell command
         format!("{}", String::from_utf8_lossy(&output.stdout))
@@ -53,7 +64,7 @@ fn exec(cmd: String, input: String) -> String {
     }
 }
 
-fn readline(reader: &mut BufRead, line: &str, args: &Vec<String>) -> String {
+fn readline(reader: &mut BufRead, line: &str, args: &[String]) -> String {
     let p = line.trim_right().len(); // Position of '\n' in string
     if p == 0 {
         panic!("{}", err("a shell command should follow #!"));
@@ -62,16 +73,17 @@ fn readline(reader: &mut BufRead, line: &str, args: &Vec<String>) -> String {
     let mut c = if &cmd[p - 1..] == "\\" {
         // if line is ended by '\' character
         let mut line = String::new();
-        reader
-            .read_line(&mut line)
-            .expect(&err("couldn't read the input file buffer")); // PANIC
+        reader.read_line(&mut line).unwrap_or_else(|_| {
+            let msg = err("couldn't read the input file buffer");
+            panic!(msg)
+        });
         String::from(&cmd[..p - 1]) + &readline(reader, &line, &args)
     } else {
         cmd
     }; // Return inlined command
     c = str::replace(&c, "$*", &args[2..].join(" "));
-    for i in 2..args.len() {
-        c = str::replace(&c, &format!("${}", i - 1), &args[i]);
+    for (i, arg) in args.iter().enumerate().skip(2) {
+        c = str::replace(&c, &format!("${}", i - 1), arg);
     }
     c // Allow sub-scripts to read command line arguments
 }
@@ -89,8 +101,8 @@ fn split2(s: &str, b: &str) -> (String, String) {
 
 fn yeast(
     reader: &mut BufRead,
-    args: &Vec<String>,
-    context: std::option::Option<std::ffi::OsString>,
+    args: &[String],
+    context: &std::option::Option<std::ffi::OsString>,
 ) -> String {
     let mut buffer = String::new();
     let mut vec = vec![];
@@ -106,13 +118,13 @@ fn yeast(
                 Some(x) => x.into_string().unwrap(),
                 None => readline(reader, &begin.1, &args),
             };
-            let end = split2(&yeast(reader, &args, None), "!#");
+            let end = split2(&yeast(reader, &args, &None), "!#");
             buffer = end.clone().1;
             vec.push(
                 std::thread::Builder::new()
                     .name(cmd.clone())
                     .spawn(move || -> String {
-                        begin.0 + &exec(cmd, end.0) // Launch command process
+                        begin.0 + &exec(&cmd, end.0) // Launch command process
                     }),
             );
         } else if buffer.contains("!#") || len == 0 {
@@ -121,15 +133,19 @@ fn yeast(
             for child in vec {
                 // Join all child threads
                 outputs += &child
-                    .expect(&err("can't unwrap named thread"))
+                    .unwrap_or_else(|_| {
+                        let msg = err("can't unwrap named thread");
+                        panic!(msg)
+                    })
                     .join()
                     .unwrap(); // PANIC
             }
             return outputs + &buffer;
         }
-        len = reader
-            .read_line(&mut buffer)
-            .expect(&err("couldn't read the input file buffer")); // PANIC
+        len = reader.read_line(&mut buffer).unwrap_or_else(|_| {
+            let msg = err("couldn't read the input file buffer");
+            panic!(msg)
+        });
     }
 }
 
@@ -147,9 +163,11 @@ fn main() {
         )
         .get_matches();
     let args: Vec<String> = std::env::args().collect();
-    let file = std::fs::File::open(std::path::Path::new(&args[1]))
-        .expect(&err(&format!("couldn't open: {}", &args[1])));
+    let file = std::fs::File::open(std::path::Path::new(&args[1])).unwrap_or_else(|_| {
+        let msg = err(&format!("couldn't open: {}", &args[1]));
+        panic!(msg)
+    });
     let mut reader = std::io::BufReader::new(file);
     let context = std::env::var_os("YEAST_CONTEXT");
-    print!("{}", split2(&yeast(&mut reader, &args, context), "!#").0);
+    print!("{}", split2(&yeast(&mut reader, &args, &context), "!#").0);
 }
